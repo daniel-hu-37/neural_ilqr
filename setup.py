fc334bbdcd9643b26e9c0875a574496fa2b568a8
bill <bill-david@163.com>
2024-02-28 22:43:00 +0800

    reorg

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ INSTALL_REQUIRES = [
     "numpy>=1.16.3", "scipy>=1.2.1", "jax>=0.2.17", "jaxlib>=0.1.71"
 ]
 
-setup(name="ilqr",
+setup(name="neural_ilqr",
       version="1.0",
       description="Auto-differentiated Iterative Linear Quadratic Regulator",
       long_description=read("README.rst"),
-      packages=find_packages(),
-      zip_safe=True,
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
       install_requires=INSTALL_REQUIRES)
